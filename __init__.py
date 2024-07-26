f8282584c91d04f6a8196c10346cb28ec9e2359e
Jeonseungwoo1 <149984505+Jeonseungwoo1@users.noreply.github.com>
2024-07-26 17:04:13 +0900

    second commit

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
-import torch
-from transformers import AutoModelForCausalLM, AutoProcessor
-from datasets import load_datasett
-from PIL import Image
-import requests
+from .config import DEVICE, MODEL_ID, BATCH_SIZE, NUM_EPOCHS, LEARNING_RATE
+from .dataset import DocVQADataset, collate_fn, get_dataloader
+from .model import load_model
+from .train import train_model
+from .inference import run_example
